6e9f0617062dcac7cfbf0216a7ac442a9b9dd82f
Balazs9 <sempergerbalazs@gmail.com>
2022-01-07 22:33:18 +0000

    add post

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Post
+from .forms import CommentForm
 
 # Create your views here.
 
@@ -27,6 +28,32 @@ class PostDetail(View):
             {
                 "post":  post,
                 "comments": comments,
+                "comment_form": CommentForm(),
+                "liked": liked
+            }
+        )
+
+    def post(self, request, slug, *args, **kwargs):
+
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('-created_on')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked=True
+
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+        
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post":  post,
+                "comments": comments,
+                "comment_form": CommentForm(),
                 "liked": liked
             }
         )
